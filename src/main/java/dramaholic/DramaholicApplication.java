e2066329d4a657d19a21f618f9d9fae8d66cd569
RenuzitV <nvminhduy2002@gmail.com>
2022-03-16 13:45:29 +0700

    initial commit

---
+++ src/main/java/dramaholic/DramaholicApplication.java
@@ -0,0 +1,13 @@
+package dramaholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaholicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaholicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dramaholic/DramaholicApplicationTests.java
@@ -0,0 +1,13 @@
+package dramaholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramaholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
