2deb4081291600c4acb3088609c426526fff37ff
RenuzitV <nvminhduy2002@gmail.com>
2022-03-24 13:46:43 +0700

    UN-37 big commit
    pretty much done with application setup, just need to add more attributes for `customer` and `movie` table

---
+++ src/main/java/dramaholic/customer/Customer.java
@@ -0,0 +1,68 @@
+package dramaholic.customer;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@EnableAutoConfiguration
+public class Customer {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private Long age;
+
+    public Customer() {
+    }
+
+    public Customer(String name, Long age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id.equals(customer.id) && name.equals(customer.name) && Objects.equals(age, customer.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+}
