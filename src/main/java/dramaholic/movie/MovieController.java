0457eaee7cfd5a2f8908c79c9b89521dd108041d
RenuzitV <nvminhduy2002@gmail.com>
2022-04-19 13:59:25 +0700

    UN-37 testing?

--- src/main/java/dramaholic/movie/MovieController.java
+++ src/main/java/dramaholic/movie/MovieController.java
@@ -1,6 +1,5 @@
 package dramaholic.movie;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
