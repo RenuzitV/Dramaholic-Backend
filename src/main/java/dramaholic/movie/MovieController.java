abcf8ecbf8c40b1bd6955894e02c58f9cf3d6d66
RenuzitV <nvminhduy2002@gmail.com>
2022-04-19 13:50:54 +0700

    UN-37 omg ezzezez

--- src/main/java/dramaholic/movie/MovieController.java
+++ src/main/java/dramaholic/movie/MovieController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
