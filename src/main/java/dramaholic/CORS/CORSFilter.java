913dd959e9fc20dffd95f620b0c40920251bc6e8
RenuzitV <nvminhduy2002@gmail.com>
2022-04-25 16:43:04 +0700

    UN-37
    trying out new CORS filter

--- src/main/java/dramaholic/CORS/CORSFilter.java
+++ src/main/java/dramaholic/CORS/CORSFilter.java
@@ -1,6 +1,10 @@
 package dramaholic.CORS;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +23,26 @@ public class CORSFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
 
         res.addHeader("Access-Control-Allow-Origin", "*");
+
         chain.doFilter(req, res);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
