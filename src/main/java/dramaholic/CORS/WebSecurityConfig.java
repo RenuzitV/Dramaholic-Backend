c68e4e949b9ba0002d61bed17e730254bbb980ec
RenuzitV <nvminhduy2002@gmail.com>
2022-04-25 16:56:15 +0700

    UN-37
    trying out new CORS filter

--- src/main/java/dramaholic/CORS/WebSecurityConfig.java
+++ src/main/java/dramaholic/CORS/WebSecurityConfig.java
@@ -18,7 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors();
+        http.cors().and()
+                .csrf();
     }
 
     @Bean
